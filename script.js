d936161dd2eff8ed821fa11aec544dc834003433
Kaique Correa <140029352+kaiqcorrea@users.noreply.github.com>
2023-10-02 23:38:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -120,6 +120,17 @@ function clearToastTimeout() {
 }
 
 /* SCRIPT GOOGLE ANALYTICS */
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtag/js?id=G-MFGYPJ17VK" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-XXXX");
+
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
